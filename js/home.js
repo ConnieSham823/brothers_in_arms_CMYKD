e3ceb8eec4207bee0769be6904c01a0fb292c8f6
keiith001 <kith.richrd@gmail.com>
2025-03-02 15:21:06 -0500

    Fade-In Function
    This commit is to create the Fade-In Function and converting current JS to JS Modules.

---
+++ js/home.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+import { videoPlayer } from "./modules/player.js";
+import { fadeInTransition } from "./modules/fadein.js";
+import { navigationMenu } from "./modules/navigation.js"
+import { generalCarousel } from "./modules/general-carousel.js";
+
+console.log("Homepage JS is Connected");
+
+// Initialize modules
+videoPlayer();
+fadeInTransition();
+navigationMenu();
+generalCarousel();
