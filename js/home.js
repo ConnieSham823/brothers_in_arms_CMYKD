29541d72023eb9a3fc16231017686de912e270d2
nnmhd <natchanone@gmail.com>
2025-03-30 18:16:51 -0400

    JS updated
    Here JS has been updated

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 import { videoPlayer } from "./modules/player.js";
 import { fadeInTransition } from "./modules/fadein.js";
-import { navigationMenu } from "./modules/navigation.js"
+import { navigationMenu } from "./modules/navigation.js";
 import { generalCarousel } from "./modules/general-carousel.js";
 import { imgZoom } from "./modules/img-zoom.js";
 
@@ -14,3 +14,27 @@ fadeInTransition();
 navigationMenu();
 generalCarousel();
 imgZoom();
+
+const app = Vue.createApp({
+  created() {
+    fetch("http://localhost:8888/BrothersInArms_API/public/blogs")
+      .then((response) => response.json())
+      .then((data) => {
+        this.blogs = data;
+        this.title = data.title;
+        this.isLoading = false;
+        console.log("Connected", data);
+      })
+      .catch((error) => {
+        console.error("Error fetching blogs:", error);
+      });
+  },
+  data() {
+    return {
+      blogs: [],
+      title: "",
+      isLoading: true,
+    };
+  },
+  methods: {},
+}).mount("#app");
